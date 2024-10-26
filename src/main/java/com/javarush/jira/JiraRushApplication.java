d60998e7b60d84dd5afba31095691ebf881f8287
eugene.kavalchuk@gmail.com <eugene@air.local>
2024-10-26 17:20:12 +0400

    Add .env file settings

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -1,17 +1,22 @@
 package com.javarush.jira;
 
 import com.javarush.jira.common.internal.config.AppProperties;
+import com.javarush.jira.common.internal.config.DataSourceProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
+@EnableConfigurationProperties({AppProperties.class, DataSourceProperties.class})
 @EnableCaching
 public class JiraRushApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(JiraRushApplication.class, args);
     }
 }
---
+++ src/main/java/com/javarush/jira/common/internal/config/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.javarush.jira.common.internal.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class DataSourceConfig {
+    private final DataSourceProperties dataSourceProperties;
+
+    @Bean
+    @Profile("prod")
+    public DataSource postgresDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(dataSourceProperties.getDriverClassName())
+                .url(dataSourceProperties.getUrl())
+                .username(dataSourceProperties.getUsername())
+                .password(dataSourceProperties.getPassword())
+                .build();
+    }
+}
